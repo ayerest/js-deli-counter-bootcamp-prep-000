cc6fdc6e4dc2d0062db6abcc50544c90c87a6907
Iris S <irisiseve@gmail.com>
2019-05-16 00:15:54 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function takeANumber(deli_line, name) {
+  deli_line.push(name);
+  return `Welcome, ${name}. You are number ${deli_line.length} in line.`
+}
+
+function nowServing(deli_line) {
+  if (deli_line.length > 0) {
+    
+  } else {
+    
+  }
+}
