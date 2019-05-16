448b53e9e00350886890f1ff06884463324fea4a
Iris S <irisiseve@gmail.com>
2019-05-16 00:18:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber(deli_line, name) {
 function nowServing(deli_line) {
   if (deli_line.length > 0) {
     return `Currently serving ${deli_line[0]}.`
-    deli_line.shift(); 
+    deli_line.shift();
   } else {
     return "There is nobody waiting to be served!";
   }
